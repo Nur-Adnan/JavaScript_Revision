b044eda843b30d76568721a22024fe907ed47a02
Nur_Adnan <nuradnanchowdhuryestobdho@gmail.com>
2023-01-21 11:51:08 +0600

    using type of function

--- revision.js
+++ revision.js
@@ -15,5 +15,18 @@ let job1 = "programmer";
 let job2 = "problem solver";
 console.log(myFirstJob);
 
+// Data Types
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+// console.log(typeof true);
+console.log(typeof javascriptIsFun);
+javascriptIsFun = 'YES!';
+console.log(typeof javascriptIsFun);
+let year;
+console.log(year);
+console.log(typeof year);
+year = 1991;
+console.log(typeof year);
+console.log(typeof null);
 
 
