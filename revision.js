7f58c3f6fc13dd83cd3f0fd329c224e561d8a9e9
Nur_Adnan <nuradnanchowdhuryestobdho@gmail.com>
2023-01-21 13:03:26 +0600

    Equality Operators: == vs. ===

--- revision.js
+++ revision.js
@@ -208,3 +208,25 @@ if (height) {
 } else {
   console.log('Height is UNDEFINED');
 }
+
+// Equality Operators: == vs. ===
+const latest_age = '18';
+if (age === 18) 
+    console.log('You just became an adult :D (strict)');
+if (age == 18) 
+    console.log('You just became an adult :D (loose)');
+
+
+const favourite = Number(prompt("What's your favourite number?"));
+console.log(favourite);
+console.log(typeof favourite);
+
+if (favourite === 23) { // 22 === 23 -> FALSE
+  console.log('Cool! 23 is an amzaing number!')
+} else if (favourite === 7) {
+  console.log('7 is also a cool number')
+} else if (favourite === 9) {
+  console.log('9 is also a cool number')
+} else {
+  console.log('Number is not 23 or 7 or 9')
+}
