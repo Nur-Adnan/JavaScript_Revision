b330171c4093f7dbc2518ffdb01c133ba4b2f9c7
Nur_Adnan <nuradnanchowdhuryestobdho@gmail.com>
2023-01-21 11:49:14 +0600

    all things of variables are revised

---
+++ revision.js
@@ -0,0 +1,19 @@
+'use strict';
+
+// Values and Variables
+let js = "amazing";
+console.log(js);
+
+// Variable name conventions
+let nur_Adnan = "JS";
+let $function = 25;
+let person = "jonas";
+let PI = 3.1415;
+let myFirstJob = "Coder";
+let myCurrentJob = "Teacher";
+let job1 = "programmer";
+let job2 = "problem solver";
+console.log(myFirstJob);
+
+
+
