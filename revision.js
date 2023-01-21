adf331bed7d21dd9b5ce58e8830216eb0ea84250
Nur_Adnan <nuradnanchowdhuryestobdho@gmail.com>
2023-01-21 12:56:07 +0600

    Type Conversion and Coercion

--- revision.js
+++ revision.js
@@ -166,3 +166,21 @@ if (BMI_nur > BMI_neha) {
   console.log(`Neha's BMI (${BMI_neha}) is higher than Nur's (${BMI_nur})!`)
 }
 
+// Type Conversion and Coercion
+
+// type conversion
+const inputYear = '1991';
+console.log(Number(inputYear), inputYear);
+console.log(Number(inputYear) + 18);
+console.log(Number('Nur'));
+console.log(typeof NaN);
+console.log(String(23), 23);
+
+
+// type coercion
+console.log('I am ' + 23 + ' years old');
+console.log('23' - '10' - 3);
+console.log('23' / '2');
+let n = '1' + 1; // '11'
+n = n - 1;
+console.log(n);
