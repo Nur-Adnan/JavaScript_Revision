af97251ee7eb21cd8facd47c3c0b95e678fd2e35
Nur_Adnan <nuradnanchowdhuryestobdho@gmail.com>
2023-01-21 11:58:45 +0600

    using math operators in strings

--- revision.js
+++ revision.js
@@ -52,3 +52,7 @@ const ageSarah = now - 2018;
 console.log(ageJonas, ageSarah);
 console.log(ageJonas * 2, ageJonas / 10, 2 ** 3);
 // 2 ** 3 means 2 to the power of 3 = 2 * 2 * 2
+
+const firstName = 'Nur';
+const lastName = 'Adnan';
+console.log(firstName + ' ' + lastName);
