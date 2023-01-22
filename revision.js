cebb23497cab55fbe5bda1ef221da42659577bf4
Nur_Adnan <nuradnanchowdhuryestobdho@gmail.com>
2023-01-22 20:11:53 +0600

    Functions

--- revision.js
+++ revision.js
@@ -352,3 +352,28 @@ console.log(`The bill was ${bill}, the tip was ${tip}, and the total value ${bil
 
 
 // Activating Strict Mode
+
+let hasDriveringLicense = false;
+const passTest = true;
+if (passTest) 
+  hasDriveringLicense = true;
+if (hasDriveringLicense) 
+  console.log('I can drive :D');
+
+
+// Functions
+
+function logger() {
+  console.log('My name is Jonas');
+}
+// calling / running / invoking function
+logger();
+function fruitProcessor(apples, oranges) {
+  const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
+  return juice;
+}
+const appleJuice = fruitProcessor(5, 0);
+console.log(appleJuice);
+const appleOrangeJuice = fruitProcessor(2, 4);
+console.log(appleOrangeJuice);
+const num = Number('23');
